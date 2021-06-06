ead37dad54308e31ea62577be1b3b43690bf9cf5
rds80 <rippleshah@msn.com>
2021-06-06 11:38:39 -0400

    Added CoVarianceAndContraVariance project

---
+++ IntroducingGenerics/CoVarianceAndContraVariance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoVarianceAndContraVariance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
